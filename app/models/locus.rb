654112b8d1df504ca67fdf2fcda134b3696ebd7a
William Mathewson <wncmathewson@me.com>
2014-01-22 16:47:37 +0000

    Added in support for geocode

--- app/models/locus.rb
+++ app/models/locus.rb
@@ -1,2 +1,10 @@
 class Locus < ActiveRecord::Base
+
+  geocoded_by :address, latitude: :lat, longitude: :long
+
+  after_validation :geocode
+
+  validates :title, presence: true
+  validates :address, presence: true
+
 end
